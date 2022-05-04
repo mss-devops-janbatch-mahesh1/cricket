9ae2d61d5b55ff1c02f86e2e5eefce9e86f1d841
maheshlucky0202 <pittalamahesh397@gmail.com>
2022-05-04 17:08:08 +0530

    git commands

---
+++ inutils.java
@@ -0,0 +1,16 @@
+this is java mang
+
+mkdir crickbuz
+cd cricbuxx
+git init
+git status
+ create a file
+ git add
+ git coomit -m
+ git config --golbal username= "'
+ git config  --global email
+ git push
+git log 
+git log -2 
+git show 
+git show --pretty="" --name -only
